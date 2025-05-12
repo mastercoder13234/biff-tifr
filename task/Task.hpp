bf06928ef372c41884f19b539a43c7d9f64a28a3
mastercoder13234 <158340518+mastercoder13234@users.noreply.github.com>
2025-05-12 15:51:04 +0000

    Stuff

--- task/Task.hpp
+++ task/Task.hpp
@@ -1,1 +1,15 @@
 #pragma once
+
+#include <string>
+using namespace std;
+
+class Task {
+    public: 
+        Task(string& name, string& desc);
+        Task(string& name, string& desc, uint8_t& priority);
+        Task(string& name, string& desk, uint32_t& due_date);
+        Task(string& name, string& desc, uint8_t& priority, uint32_t& due_date);
+        string getName() const;
+        void setName(string& name);
+
+};
